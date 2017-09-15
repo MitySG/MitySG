a483eea59a63000f7c3675a383bdb54426df2feb
riwu <riwu0730@gmail.com>
2017-09-15 18:02:33 +0800

    added search functionality

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import Lists from './Lists';
 
 class Search extends React.Component {
+  state = {
+    searchValue: '',
+    busFilter: '',
+  }
   render() {
-    const list = this.props.buses.reduce((list, bus) => list.concat(bus.route), []);
+    console.log(this.state.busFilter, this.state.busFilter.trim());
+    const list = this.props.buses
+      .filter(bus => !this.state.busFilter ||
+        this.state.busFilter.trim().toLowerCase() === bus.busNumber.toLowerCase())
+      .reduce((list, bus) => list.concat(bus.route), [])
+      .filter(busStop => busStop.toLowerCase().includes(this.state.searchValue.trim().toLowerCase()));
     return (
       <div >
         <input
           type="search"
           placeholder="Search"
+          onChange={e => this.setState({ searchValue: e.target.value })}
         />
         <input
           type="search"
           placeholder="Filter by bus number"
+          onChange={e => this.setState({ busFilter: e.target.value })}
         />
         <Lists list={list} />
       </div>
