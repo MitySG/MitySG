24ad4882d2a5f2272074cee50040e0190727ac26
riwu <riwu0730@gmail.com>
2017-09-15 22:23:58 +0800

    reformat bus state tree

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -7,15 +7,15 @@ class Search extends React.Component {
     busFilter: '',
   }
   render() {
-    const searchValue = this.state.searchValue.trim();
-    const list = this.props.buses
-      .filter(bus => !this.state.busFilter ||
-        this.state.busFilter.trim().toLowerCase() === bus.busNumber.toLowerCase())
-      .reduce((list, bus) => list.concat(bus.route), [])
-      .map(busStopCode => ({ ...this.props.busStops[busStopCode], code: busStopCode }))
-      .filter(busStop => busStop.description &&
-        (busStop.description.toLowerCase().includes(searchValue.toLowerCase()) ||
-         busStop.code.includes(searchValue)));
+    const searchValue = this.state.searchValue.trim().toLowerCase();
+    const busFilter = this.state.busFilter.trim();
+    const busStops = (busFilter
+      ? (this.props.buses[busFilter.toLowerCase()] || this.props.buses[busFilter.toUpperCase()] || [])
+          .map(code => ({ ...this.props.busStops[code], code }))
+      : Object.entries(this.props.busStops).map(([code, busStop]) => ({ ...busStop, code })))
+        .filter(busStop => busStop.description &&
+          (busStop.description.toLowerCase().includes(searchValue) ||
+           busStop.code.includes(searchValue)));
     return (
       <div >
         <input
@@ -28,7 +28,7 @@ class Search extends React.Component {
           placeholder="Filter by bus number"
           onChange={e => this.setState({ busFilter: e.target.value })}
         />
-        {list.length === 0 ? <div>No results</div> : <Lists list={list} />}
+        {busStops.length === 0 ? <div>No results</div> : <Lists list={busStops} />}
 
       </div>
     );
