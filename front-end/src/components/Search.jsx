cc82e2f95640c6e65e49f81c81a304fafc3dda7b
riwu <riwu0730@gmail.com>
2017-09-17 12:47:42 +0800

    Add select field

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
-import Lists from './Lists';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import './Search.css';
 
 class Search extends React.Component {
   state = {
-    searchValue: '',
-    busFilter: '',
+    selectedBus: '96',
+    selectedStart: undefined,
+    selectedEnd: undefined,
   }
   render() {
-    const searchValue = this.state.searchValue.trim().toLowerCase();
-    const busFilter = this.state.busFilter.trim();
-    const busStops = (busFilter
-      ? (this.props.buses[busFilter.toLowerCase()] || this.props.buses[busFilter.toUpperCase()] || [])
-          .map(code => ({ ...this.props.busStops[code], code }))
-      : Object.entries(this.props.busStops).map(([code, busStop]) => ({ ...busStop, code })))
-        .filter(busStop => busStop.description &&
-          (busStop.description.toLowerCase().includes(searchValue) ||
-           busStop.code.includes(searchValue)));
+    const busStopOptions = (this.props.buses[this.state.selectedBus] || [])
+      .map((code) => {
+        const description = (this.props.busStops[code] || {}).description;
+        return {
+          value: description,
+          label: description,
+        };
+      });
     return (
-      <div >
-        <input
-          type="search"
-          placeholder="Search"
-          onChange={e => this.setState({ searchValue: e.target.value })}
-        />
-        <input
-          type="search"
-          placeholder="Filter by bus number"
-          onChange={e => this.setState({ busFilter: e.target.value })}
-        />
-        {busStops.length === 0 ? <div>No results</div> : <Lists list={busStops} />}
-
+      <div className="Search">
+        <div className="busSearch">
+          Bus
+          <Select
+            className="busSelect"
+            clearable={false}
+            value={this.state.selectedBus}
+            onChange={selectedBus => this.setState({ selectedBus: selectedBus.value })}
+            options={Object.keys(this.props.buses).map(bus => ({
+              value: bus,
+              label: bus,
+            }))}
+          />
+        </div>
+        <div className="busSearch">
+          Start
+          <Select
+            className="busSelect"
+            clearable={false}
+            value={this.state.selectedStart}
+            onChange={selectedStart => this.setState({ selectedStart: selectedStart.value })}
+            options={busStopOptions}
+          />
+        </div>
+        <div className="busSearch">
+          Stop
+          <Select
+            className="busSelect"
+            clearable={false}
+            value={this.state.selectedEnd}
+            onChange={selectedEnd => this.setState({ selectedEnd: selectedEnd.value })}
+            options={busStopOptions}
+          />
+        </div>
       </div>
     );
   }
