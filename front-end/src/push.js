29e833f6f62cdbbe151fbb368c72b26bf8110d26
Han <leook0209@gmail.com>
2017-09-20 20:44:57 +0800

    remove log

--- front-end/src/push.js
+++ front-end/src/push.js
@@ -10,23 +10,16 @@ function buildApplicationServerKey() {
   const base64 = 'BBFNCDSVirGYfrhSp5Q1u-ksUTnn7HMUnigBiZ0dvYdsNjtL4OtlRjuf4Gki55eZohpa-FiWFOvUIVI7UKRucx0';
   const rfc4648 = base64.replace(/-/g, '+').replace(/_/g, '/');
   const characters = atob(rfc4648).split('').map(character => character.charCodeAt(0));
-  console.log("PUBLIC KEY");
-  console.log(base64);
   return new Uint8Array(characters);
 }
 
 function sendSubscriptionToServer(subscription) {
-  // This is where you'd update the subscription on the server.
-  document.querySelector('.js-subscription').innerHTML = JSON.stringify(subscription.toJSON());
-
-  let xhr = new XMLHttpRequest();
-  // let url = "http://localhost:8000";
-  // let url = "https://zjh5q71uma.execute-api.ap-southeast-1.amazonaws.com/prod/testfunction";
-  let url = "https://huy3vicolc.execute-api.us-east-1.amazonaws.com/dev/subscribe";
+  const xhr = new XMLHttpRequest();
+  const url = "https://huy3vicolc.execute-api.us-east-1.amazonaws.com/dev/subscribe";
   xhr.open("POST", url, true);
   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   xhr.send(JSON.stringify(subscription.toJSON()));
-  console.log("SENT")
+  console.log("Subscription Sent")
   console.log(JSON.stringify(subscription.toJSON()));
   xhr.onload = function () {
     console.log(JSON.parse(this.responseText));
