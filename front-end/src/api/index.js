aaf63d24055e197f89a3c30ef3b1db564d0b0292
riwu <riwu0730@gmail.com>
2017-09-23 13:08:21 +0800

    get bus and bus stops from backend

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,18 +1,11 @@
-import buses from './buses.json';
-import busStops from './busStops.json';
-
-// const [get, post] = ['GET', 'POST'].map(method =>
-//   path => fetch(process.env.REACT_APP_API_BASE_URL + path, {
-//     headers: { 'Content-Type': 'application/json' },
-//     method,
-//   }).then((response) => { console.log(response); return response.json(); })
-//     .catch(err => console.log(err)),
-// );
-
-// https://zihgyx1zp8.execute-api.ap-southeast-1.amazonaws.com/prod/BusServices
-// https://q5nyw1vwna.execute-api.ap-southeast-1.amazonaws.com/prod/BusStops
+const [get, post] = ['GET', 'POST'].map(method =>
+  path => fetch(path, {
+    method,
+  }).then(response => response.json())
+    .catch(err => console.log(err)),
+);
 
 export default {
-  getBuses: () => Promise.resolve(buses),
-  getBusStops: () => Promise.resolve(busStops),
+  getBuses: () => get('https://zihgyx1zp8.execute-api.ap-southeast-1.amazonaws.com/prod/BusServices'),
+  getBusStops: () => get('https://q5nyw1vwna.execute-api.ap-southeast-1.amazonaws.com/prod/BusStops'),
 };
