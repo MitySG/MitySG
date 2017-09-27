3237a45798abcc073ef53e5dde03061e3493d546
riwu <riwu0730@gmail.com>
2017-09-28 00:09:19 +0800

    add hot reloading

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import push from './push';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
+
 registerServiceWorker();
 push.subscribe();
