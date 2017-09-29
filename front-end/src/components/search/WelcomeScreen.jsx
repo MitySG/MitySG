d775728972ef764f2f0d0caa26a11fb7750f490b
riwu <riwu0730@gmail.com>
2017-09-29 20:53:42 +0800

    add welcomescreen

--- front-end/src/components/search/WelcomeScreen.jsx
+++ front-end/src/components/search/WelcomeScreen.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import { StepLabel } from 'material-ui/Stepper';
 import './WelcomeScreen.css';
@@ -15,19 +14,31 @@ class WelcomeScreen extends React.Component {
   }
   render() {
     return (
-      <div styleName="container">
+      <div>
         <div styleName="image" />
-        <Paper styleName="home" zDepth={3}>
-          {
-            ['Choose transit mode', 'Select start and destination', 'Sit back and enjoy'].map((text, index) => (
-              <StepLabel
-                style={{ height: '30px' }}
-                icon={index + 1}
-                active
-              >
-                {text}
-              </StepLabel>
-            ))}
+        <div styleName="title">Am I There Yet?</div>
+        <div styleName="underscore">__________</div>
+
+
+        <Paper styleName="paper" zDepth={3}>
+          <div styleName="steps">
+            {
+              ['Choose transit mode', 'Select start and destination', 'Sit back and enjoy'].map((text, index) => (
+                <StepLabel
+                  key={text}
+                  style={{ height: '30px' }}
+                  icon={index + 1}
+                  active
+                >
+                  {text}
+                </StepLabel>
+              ))}
+          </div>
+          <RaisedButton primary label="BEGIN YOUR JOURNEY" onClick={this.props.onBeginJourney} />
+          <div styleName="bottomText">
+            <div>Enjoy your worry-free ride.</div>
+            <div>{"From start to end, we'll keep you notified"}</div>
+          </div>
         </Paper>
       </div>
     );
